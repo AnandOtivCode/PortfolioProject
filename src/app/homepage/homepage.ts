685c5a999f4fd0b0058613c4852e9044084e3384
Anand Otiv <w0858857@myscc.ca>
2025-12-07 02:03:43 -0500

    Added Icons to about me

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -3,12 +3,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {Navbar} from "../navbar/navbar";
+import {MatCard} from '@angular/material/card';
 
 @Component({
   selector: 'app-homepage',
-    imports: [MatToolbarModule,
-        MatButtonModule,
-        MatIconModule, Navbar],
+  imports: [MatToolbarModule,
+    MatButtonModule,
+    MatIconModule, Navbar, MatCard],
   templateUrl: './homepage.html',
   styleUrl: './homepage.css',
 })
